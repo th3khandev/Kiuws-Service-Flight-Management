de86697ce6fa0aa3e8541a959a493bfa5e5b9941
Anthony Garcia <anthonydeyvis32@gmail.com>
2023-10-04 12:05:46 -0400

    feat(): Add description

---
+++ kiuws-service-flight-management.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Kiuws Service Flight Management
+ * Description: Streamline flight management on your site. Connect to Kiuws API for real-time flight data. Perfect for travel websites.
+ * Version: 1.0
+ * Author: Anthony Garcia - @th3khandev
+ */
+
+// Aquí irán las funciones y código del plugin.
