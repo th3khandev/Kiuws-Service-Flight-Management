23488a93414029779a9955bfad9640d824d90d89
Anthony Garcia <anthonydeyvis32@gmail.com>
2023-10-04 15:23:42 -0400

    feat(): Create plugin constants

--- kiuws-service-flight-management.php
+++ kiuws-service-flight-management.php
@@ -6,6 +6,13 @@
  * Author: Anthony Garcia - @th3khandev
  */
 
+ // Define constants
+define('FLIGHT_MANAGEMENT_VERSION', '1.0');
+define('FLIGHT_MANAGEMENT_DIR', plugin_dir_path(__FILE__));
+define('FLIGHT_MANAGEMENT_URL', plugin_dir_url(__FILE__));
+define('FLIGHT_MANAGEMENT_FILE', __FILE__);
+define('FLIGHT_MANAGEMENT_PREFIX', 'flight_management_');
+
 // This function adds a page to the WordPress admin menu
 function add_admin_menu_page() {
     add_menu_page(
