9df94bfe8cd5b8c1a624b8578169119746d50cd3
Anthony Garcia <anthonydeyvis32@gmail.com>
2023-10-06 15:30:48 -0400

    feat(): Create shortcode with search form

--- kiuws-service-flight-management.php
+++ kiuws-service-flight-management.php
@@ -146,3 +146,54 @@ function save_flight_configuration() {
     }
 }
 add_action('admin_init', 'save_flight_configuration');
+
+// Add the shortcode to display the flight search form
+function generate_flight_search_form() {
+    ob_start();
+    // Create the form
+    ?>
+    <form id="flight-search-form">
+        <div class="row">
+            <!-- Add fields -->
+            <!-- Origin -->
+            <div class="col-12 col-sm-12 col-md-3 col-lg-3">
+                <label for="origin" class="form-label">Desde: </label>
+                <select name="origin" placeholder="Origen" class="form-control"></select>
+            </div>
+            <!-- Destination -->
+            <div class="col-12 col-sm-12 col-md-3 col-lg-3">
+                <label for="destination" class="form-label">Destino: </label>
+                <select name="destination" placeholder="Destino" class="form-control"></select>
+            </div>
+            <!-- Departure date -->
+            <div class="col-12 col-sm-12 col-md-3 col-lg-3">
+                <label for="departure_date" class="form-label">Fecha de salida: </label>
+                <input type="date" name="departure_date" placeholder="Fecha de salida" class="form-control" />
+            </div>
+            <!-- Amount Adults and children --> 
+            <div class="col-12 col-sm-12 col-md-3 col-lg-3">
+                <div class="row">
+                    <div class="col-12 col-sm-12 col-md-6 col-lg-6">
+                        <label for="amount_adults" class="form-label">Adultos: </label>
+                        <input type="number" name="amount_adults" placeholder="Adultos" class="form-control" value="1"/>
+                    </div>
+                    <div class="col-12 col-sm-12 col-md-6 col-lg-6">
+                        <label for="amount_children" class="form-label">Niños: </label>
+                        <input type="number" name="amount_children" placeholder="Niños" class="form-control" value="0" />
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-12 text-center mt-5">
+                <button type="submit" class="btn btn-primary">Buscar vuelos</button>
+            </div>
+        </div>
+    </form>
+    <?php
+    return ob_get_clean();
+}
+
+// Register the shortcode
+function flight_search_form_shortcode() {
+    return generate_flight_search_form();
+}
+add_shortcode('flight_search', 'flight_search_form_shortcode');
