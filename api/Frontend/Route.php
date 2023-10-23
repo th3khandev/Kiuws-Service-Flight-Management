458ca8033902d90a7b9a3c4e5a56627b0fb93d8a
Anthony Garcia <anthonydeyvis32@gmail.com>
2023-10-23 11:47:22 -0400

    feat(): Create service get flights available

--- api/Frontend/Route.php
+++ api/Frontend/Route.php
@@ -2,6 +2,7 @@
 
 namespace Kiuws_Service_Flight_Management\Api\Frontend;
 
+use Kiuws_Service_Flight_Management\Services\Kiuws;
 use Kiuws_Service_Flight_Management\Services\OpenFlightsOrg;
 use WP_REST_Controller;
 use WP_REST_Server;
@@ -32,6 +33,18 @@ class Route extends WP_REST_Controller
                 ]
             ]
         );
+
+        register_rest_route(
+            $this->namespace,
+            'get-flight-available',
+            [
+                [
+                    'methods' => WP_REST_Server::READABLE,
+                    'callback' => [$this, 'get_flight_available'],
+                    'permission_callback' => [$this, 'get_route_access'],
+                ]
+            ]
+        );
     }
 
     /**
@@ -52,6 +65,31 @@ class Route extends WP_REST_Controller
 
         $open_flight_org = new OpenFlightsOrg();
         $response = $open_flight_org->getAirports($search);
+
+        return rest_ensure_response($response);
+    }
+
+    /**
+     * Get flight available
+     */
+    public function get_flight_available($request) {
+        $params = $request->get_params();
+        $origin = $params['origin'];
+        $destination = $params['destination'];
+        $departure_date = $params['depurate_date'];
+        $adults = $params['adults'];
+        $children = $params['children'];
+
+        // Get configuration from options
+        $base_url = get_option(FLIGHT_MANAGEMENT_PREFIX . 'base_url');
+        $agent_sine = get_option(FLIGHT_MANAGEMENT_PREFIX . 'agent_sine');
+        $terminal_id = get_option(FLIGHT_MANAGEMENT_PREFIX . 'terminal_id');
+        $user = html_entity_decode(get_option(FLIGHT_MANAGEMENT_PREFIX . 'user'));
+        $password = html_entity_decode(get_option(FLIGHT_MANAGEMENT_PREFIX . 'password'));
+        $mode = get_option(FLIGHT_MANAGEMENT_PREFIX . 'mode');
+
+        $kiuws = new Kiuws($base_url, $agent_sine, $terminal_id, $user, $password, $mode);
+        $response = $kiuws->getAvailabilityFlights($departure_date, $origin, $destination, $adults, $children);
 
         return rest_ensure_response($response);
     }
---
+++ services/IataOrg.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace  Kiuws_Service_Flight_Management\Services;
+
+use DOMDocument;
+use DOMXPath;
+
+class IataOrg {
+    private $dirAirlineLogos = 'assets/images/airlines/';
+    private $logoDefault = '';
+    private $airlinesLogoDefault = [];
+    private $airlinesDbFile = FLIGHT_MANAGEMENT_DIR . 'db/json/airlines.json';
+
+    public function __construct()
+    {
+        $this->logoDefault = $this->dirAirlineLogos . 'default.png';
+        $this->airlinesLogoDefault = [
+            '9R'    => $this->dirAirlineLogos . '9R.png',
+            'AA'    => $this->dirAirlineLogos . 'AA.png',
+            '9V'    => $this->dirAirlineLogos . '9V.webp',
+            'KN'    => $this->dirAirlineLogos . 'KN.webp',
+            'L5'    => $this->dirAirlineLogos . 'L5.png',
+            'AV'    => $this->dirAirlineLogos . 'AV.png',
+            'LA'    => $this->dirAirlineLogos . 'LA.png',
+            'UA'    => $this->dirAirlineLogos . 'UA.png',
+        ];
+    }
+
+    public function getAirlineByCode ($code) {
+        // open file
+        $fileAirline = file_get_contents($this->airlinesDbFile);
+        // decode json to array
+        $airlines = json_decode($fileAirline, true);
+        // search airline by code
+        foreach ($airlines as $airline) {
+            if ($airline['code'] == $code) {
+                return $airline;
+            }
+        }    
+        return null;
+    }
+
+    public function addAirlineToFile ($code) {
+        // make GET to https://www.iata.org/PublicationDetails/Search/?currentBlock=314383
+        $response = wp_remote_get('https://www.iata.org/PublicationDetails/Search/?currentBlock=314383&airline.search=' . $code);
+        if (is_wp_error($response)) {
+            return null;
+        }
+        // get html
+        $html = wp_remote_retrieve_body($response);
+        // parse html
+        $dom = new DOMDocument();
+        @$dom->loadHTML($html);
+        $xpath = new DOMXPath($dom);
+        // get table with class datatable
+        $table = $xpath->query('//table[@class="datatable"]')->item(0); // <table class="datatable"><thead> <tr><td>Company name</td><td>Country / Territory</td><td>2-letter code</td></tr></thead><tbody><tr><td>Air China Cargo Co., Ltd</td><td>People&#x27;s Republic of China</td><td>CA*</td></tr> <tr> <td>Air China LTD</td><td>People&#x27;s Republic of China</td><td>CA</td></tr></tbody></table>
+        // get rows into tbody
+        $rows = $table->getElementsByTagName('tbody')->item(0)->getElementsByTagName('tr');
+        // get airline
+        $airline = [];
+        foreach ($rows as $row) {
+            $tds = $row->getElementsByTagName('td');
+            if ($tds->length == 3) {
+                $airline['name'] = $tds->item(0)->nodeValue;
+                $airline['country'] = $tds->item(1)->nodeValue;
+                $airline['code'] = $code;
+                $airline['logo'] = '';
+                if (isset($this->airlinesLogoDefault[$code])) {
+                    $airline['logo'] = $this->airlinesLogoDefault[$code];
+                } else {
+                    $airline['logo'] = $this->logoDefault;
+                }
+                break;
+            }
+        }
+        // add airline to file
+        $fileAirline = file_get_contents($this->airlinesDbFile);
+        // decode json to array
+        $airlines = json_decode($fileAirline, true);
+        // add airline
+        $airlines[] = $airline;
+        // encode array to json
+        $fileAirline = json_encode($airlines);
+        // save file
+        file_put_contents($this->airlinesDbFile, $fileAirline);
+        return $airline;
+    }
+}
